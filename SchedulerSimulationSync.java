891a1fe5893204a1c5a56664351584cb6e5a9f12
nouf-fahad-445 <445052117@std.psau.edu.sa>
2026-04-29 01:22:42 +0300

    Method to log execution updated

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -93,7 +93,12 @@ class SharedResources {
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        logLock.lock(); 
+        try { 
+          executionLog.add(message);
+        } finally { 
+            logLock.unlock(); 
+        } 
     }
 }
 
